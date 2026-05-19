027b73271d6d7a2ae5b0674eab8a3858206f4dd0
SphannajerFuentes <sphannajerfuentes@gmail.com>
2026-05-19 08:13:46 -0500

    Implementando SecurityConfig para proteccion de rutas

---
+++ src/main/java/SafeZone/SafeZoneBackend/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package SafeZone.SafeZoneBackend.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        // Rutas públicas: Login y Registro no requieren token
+                        .requestMatchers("/api/auth/**").permitAll()
+
+                        .requestMatchers("/api/victim/**").hasRole("VICTIM")
+                        .requestMatchers("/api/psychologist/**").hasRole("PSYCHOLOGIST")
+                        .requestMatchers("/api/defender/**").hasRole("DEFENDER")
+                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
+
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // ¡Crucial para JWT!
+                )
+                // Agregamos nuestro filtro de JWT antes del filtro de login de Spring
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
